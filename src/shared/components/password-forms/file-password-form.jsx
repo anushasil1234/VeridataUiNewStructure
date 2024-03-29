f25e1591223421b41ef471cceead52e9d1e187a6
elogixDibyo <dibyojit@elogixmail.com>
2024-03-29 11:19:25 +0530

    file passwordlog conflict fixing

--- src/shared/components/password-forms/file-password-form.jsx
+++ src/shared/components/password-forms/file-password-form.jsx
@@ -42,7 +42,7 @@ const { filePasswordSubmitionProps, closeFilePasswordSubmitionModel }  =props ;
             setFilePassword(value);
             setDisplayfilePassword(value);
         } catch (error) {
-            console.log('error', error);
+            // console.log('error', error);
         }
     }
 
