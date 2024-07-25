3f913e8db248960ca1e214148ccce4682d011326
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-07-25 17:43:19 +0530

    Login redux init state set

--- src/store/slices/login-slice.jsx
+++ src/store/slices/login-slice.jsx
@@ -1,17 +1,40 @@
-import { createSlice} from "@reduxjs/toolkit"
-
+import { createSlice } from "@reduxjs/toolkit"
+const loginBase = {
+    "userId": Number,
+    "userCode": String,
+    "userName": String,
+    "password": String,
+    "emailId": String,
+    "phone": String,
+    "roleId": Number,
+    "roleName": String,
+    "userTypeId": Number,
+    "companyId": Number,
+    "companyName": String,
+    "consentStatus": Number,
+    "isConsentProcessed": Boolean || null,
+    "isPrerequisiteDataAvailable": Boolean || null,
+    "isProcessed": Boolean || null,
+    "isSubmit": Boolean || null,
+    "appointeeId": Number || null,
+    "status": String,
+    "isSetProfilePassword": Boolean|| null,
+    "isDefaultPassword": Boolean|| null,
+    "isPasswordExpire": Boolean|| null
+};
 const LoggedinData = createSlice({
     name: "loggedinData",
     initialState: [],
     reducers: {
         storeLoggedinData(state, action) {
+            console.log("statestatestate", action.payload);
             state.push(action.payload);
         },
         removeLoggedinData(state, action) {
-            state.pop();
+            state.push(loginBase);
         },
     }
 })
 
 export default LoggedinData.reducer
-export const {storeLoggedinData, removeLoggedinData} = LoggedinData.actions;
+export const { storeLoggedinData, removeLoggedinData } = LoggedinData.actions;
