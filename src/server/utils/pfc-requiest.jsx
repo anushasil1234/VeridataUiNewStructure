07affc06cc9710e2c3a6a5a2a4bda92fca05b72e
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-07-25 16:39:11 +0530

    Refresh token only for admin

--- src/server/utils/pfc-requiest.jsx
+++ src/server/utils/pfc-requiest.jsx
@@ -9,10 +9,12 @@ import { removeDropdownList } from 'store/slices/dropdown-slice';
 import { removeApi } from 'store/slices/api-slice';
 import { removeLoggedinData } from 'store/slices/login-slice';
 import axios from "axios";
+import { removeLoggedinTokenData } from 'store/slices/login-token-slice';
+import { removeSideMenuItems } from 'store/slices/side-menu-items-slice';
 
 const PfcRequiest = (Component) => {
     const PfcRequiestAdded = () => {
-
+        const userDetails = getLocalStorageItem("pfc-user");
         const [loading, setLoading] = useState(false);
         const [severity, setSeverty] = useState();
         const [popUpAlertMessage, setPopUpAlertMessage] = useState();
@@ -28,12 +30,17 @@ const PfcRequiest = (Component) => {
 
         const handleClickOnLogout = () => {
             dispatch(removeLoggedinData());
+            dispatch(removeLoggedinTokenData());
             removeLocalStorageItems(["pfc-user"]);
             removeLocalStorageItems(["pfc-token"]);
             dispatch(removeApi());
             dispatch(removeDropdownList());
             dispatch(removeFunction());
             dispatch(removePopUpSetFunction());
+            dispatch(removeSideMenuItems());
+        }
+        const isAdmin = () => {
+            return userDetails.userTypeId === 1 || userDetails.userTypeId === 2;
         }
 
         const setupAxiosInterceptors = (api) => {
@@ -44,15 +51,21 @@ const PfcRequiest = (Component) => {
                     if (error.response.status === 401 && !originalRequest._retry) {
                         originalRequest._retry = true;
                         try {
-                            const tokenResponse = await refreshAuthToken();
-                            const { responseInfo } = tokenResponse.data;
-                            if (tokenResponse.status === 200) {
-                                const newTokenDetails = responseInfo;
-                                setLocalStorageItem("pfc-token", newTokenDetails);
-                                const userDetails = getLocalStorageItem("pfc-user");
-                                const customDetails = `${userDetails?.userCode}|~|${userDetails?.userId}|~|`;
-                                originalRequest.headers['Authorization'] = `Bearer ${customDetails + newTokenDetails.token}`;
-                                return api(originalRequest);
+                            if (isAdmin()) {
+                                const tokenResponse = await refreshAuthToken();
+                                const { responseInfo } = tokenResponse.data;
+                                if (tokenResponse.status === 200) {
+                                    const newTokenDetails = responseInfo;
+                                    setLocalStorageItem("pfc-token", newTokenDetails);
+
+                                    const customDetails = `${userDetails?.userCode}|~|${userDetails?.userId}|~|`;
+                                    originalRequest.headers['Authorization'] = `Bearer ${customDetails + newTokenDetails.token}`;
+                                    return api(originalRequest);
+                                }
+                            } else {
+                                handleClickOnLogout();
+                                showErrorMessage("Your session has expired. Please login again.");
+                                return Promise.reject(error);
                             }
                         } catch (e) {
                             handleClickOnLogout();
