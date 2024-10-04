d49190b6402f66365cf1fdb5226b5fb5486e1c21
pindranil <indranil.pan@elogixmail.com>
2024-10-04 16:04:39 +0530

    scroll bar issue fixed

--- src/shared/layouts/full/FullLayout.js
+++ src/shared/layouts/full/FullLayout.js
@@ -1,69 +1,56 @@
 import React, { useState } from "react";
-import { styled, Box } from '@mui/material';
-import { Outlet } from 'react-router-dom';
-
-
-// import Header from './header/Header';
-import Sidebar from './sidebar/Sidebar';
+import { styled, Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./sidebar/Sidebar";
 import ContentWrapper from "shared/utils/layout/content-wrapper";
 import CustomContainer from "shared/utils/layout/container";
 import Header from "./header/Header";
 
-const MainWrapper = styled('div')(() => ({
-  display: 'flex',
-  minHeight: '100vh',
-  width: '100%',
+const MainWrapper = styled("div")(() => ({
+  display: "flex",
+  height: "100vh", 
+  width: "100%",
 }));
 
-const PageWrapper = styled('div')(() => ({
-  display: 'flex',
-  flexGrow: 1,
+const PageWrapper = styled("div")(() => ({
+  display: "flex",
   paddingBottom: '60px',
-  flexDirection: 'column',
   zIndex: 1,
   backgroundColor: 'transparent',
+  flexGrow: 1,
+  flexDirection: "column",
+  height: "100vh",
+  
+  overflow: "hidden",
+  overflowY: "auto",
 }));
 
-const FullLayout = ({ setToken }) => {
 
+
+const FullLayout = ({ setToken }) => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
   const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
 
   return (
-    <MainWrapper
-      className='mainwrapper'
-    >
-      {/* ------------------------------------------- */}
-      {/* Sidebar */}
-      {/* ------------------------------------------- */}
-      <Sidebar isSidebarOpen={isSidebarOpen}
+    <MainWrapper  className='mainwrapper'>
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
         isMobileSidebarOpen={isMobileSidebarOpen}
-        onSidebarClose={() => setMobileSidebarOpen(false)} />
-      {/* ------------------------------------------- */}
-      {/* Main Wrapper */}
-      {/* ------------------------------------------- */}
-      <PageWrapper
-        className="page-wrapper"
-      >
-        {/* ------------------------------------------- */}
-        {/* Header */}
-        {/* ------------------------------------------- */}
-        <Header setToken={setToken} toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} toggleMobileSidebar={() => setMobileSidebarOpen(true)} />
-        {/* ------------------------------------------- */}
-        {/* PageContent */}
-        {/* ------------------------------------------- */}
-        <CustomContainer>
-          {/* ------------------------------------------- */}
-          {/* Page Route */}
-          {/* ------------------------------------------- */}
-          <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>
-            <ContentWrapper>
-              <Outlet />
-            </ContentWrapper>
+        onSidebarClose={() => setMobileSidebarOpen(false)}
+      />
+      <PageWrapper>
+        <Header
+          setToken={setToken}
+          toggleSidebar={() => setSidebarOpen(!isSidebarOpen)}
+          toggleMobileSidebar={() => setMobileSidebarOpen(true)}
+        />
+         
+        <CustomContainer >
+        <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>
+          <ContentWrapper>
+            <Outlet />
+          </ContentWrapper>
           </Box>
-          {/* ------------------------------------------- */}
-          {/* End Page */}
-          {/* ------------------------------------------- */}
         </CustomContainer>
       </PageWrapper>
     </MainWrapper>
