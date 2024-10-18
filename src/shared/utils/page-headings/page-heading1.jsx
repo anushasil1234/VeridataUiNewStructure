81c7e8ec0ea4e0c6850a93c512ff884834ee22ab
pindranil <indranil.pan@elogixmail.com>
2024-10-18 18:32:48 +0530

    add a extra heading for home page

--- src/shared/utils/page-headings/page-heading1.jsx
+++ src/shared/utils/page-headings/page-heading1.jsx
@@ -4,6 +4,14 @@ import React from 'react'
 export const PageHeading1 = ({heading}) => {
   return (
      <Typography component="h2" 
+     sx={{ fontSize: '1.2rem',justifyContent:"center" }}
+     >{heading}</Typography>
+  )
+}
+export const PageHeading2=({heading})=>{
+  return (
+     <Typography component="h1"
+     sx={{ fontSize: '0.9rem' }}
      >{heading}</Typography>
   )
 }
