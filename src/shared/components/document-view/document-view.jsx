251947028464e9b2e8f8615d66f831029ddda597
pindranil <indranil.pan@elogixmail.com>
2024-11-11 16:26:52 +0530

    add zoom in zoom out functionality

--- src/shared/components/document-view/document-view.jsx
+++ src/shared/components/document-view/document-view.jsx
@@ -1,48 +1,78 @@
-import { Download } from '@mui/icons-material';
-import { Box, Stack, Typography } from '@mui/material';
-import { actionIconStyle, fileImageHeaderStyle, fileImageStyle, fileTypeStyle, imageFileContainerStackStyle, imageFileContainerStyle, pdfFileContainerStyle } from 'app';
+import { Download , ZoomIn, ZoomOut } from '@mui/icons-material';
+import { Box, Stack, Typography,IconButton  } from '@mui/material';
+import { actionIconStyle, fileImageHeaderStyle, fileImageStyle, fileTypeStyle, imageFileContainerStackStyle, imageFileContainerStyle, pdfFileContainerStyle, zoomOutstackStyle } from 'app';
 import React from 'react'
 import { FabIcon } from 'shared/utils';
 import downloadFile from 'shared/utils/associate/download-file';
 import FabIconPropsModel from 'shared/utils/fab-icon/fab-icon-model';
 import FullScreenModel from 'shared/utils/models/fullscreen-modal'
-
+import  { useState } from 'react';
 const UnWrappedDocumentView = ({ documentModelProps }) => {
     const { fileDetails , fileType } = documentModelProps;
     const { fileName } = fileDetails;
     const mimeType = fileDetails.split(';')[0].split(':')[1];
+    const [zoomLevel, setZoomLevel] = useState(1);
+    const handleZoomIn = () => {
+        setZoomLevel((prevZoom) => Math.min(prevZoom + 0.1, 3)); 
+    };
+    const handleZoomOut = () => {
+        setZoomLevel((prevZoom) => Math.max(prevZoom - 0.1, 0.5)); 
+    };
     const downloadFabProps = new FabIconPropsModel(
         actionIconStyle,
-        ()=> downloadFile(fileDetails, fileName),
+        ()=> downloadFile(fileDetails, fileType),
         "secondary",
         "download",
         <Download />,
         "Download"
     );
     return (
-        <Stack>
-            <Stack sx={fileImageHeaderStyle}>
-                <Typography sx={fileTypeStyle}>{fileType}</Typography>
-                <FabIcon
-                    props={{
-                        ...downloadFabProps,
-                        selectedIndex: 2,
-                        index: 2,
-                        size: "small"
-                    }}
-                />
-            </Stack>
-            <Stack sx={imageFileContainerStackStyle}>
-                <Box sx={mimeType === 'application/pdf' ? pdfFileContainerStyle : imageFileContainerStyle}>
-                    {mimeType === 'application/pdf' ? 
-                    <iframe src={fileDetails} height="500" width='100%' ></iframe>
-                    :
-                    <img src={fileDetails} style={fileImageStyle} alt={fileName}/>}
-                </Box>
-            </Stack>
+        <Stack sx={{ position: 'relative' }}>
+        <Stack sx={fileImageHeaderStyle}>
+            <Typography sx={fileTypeStyle}>{fileType}</Typography>
+            <FabIcon
+                props={{
+                    ...downloadFabProps,
+                    selectedIndex: 2,
+                    index: 2,
+                    size: "small"
+                }}
+            />
         </Stack>
-    )
-}
+
+        
+        <Stack
+            sx={{...zoomOutstackStyle}}
+        >
+            <IconButton color="secondary" onClick={handleZoomOut} aria-label="zoom out">
+                <ZoomOut />
+            </IconButton>
+            <IconButton color="secondary" onClick={handleZoomIn} aria-label="zoom in">
+                <ZoomIn />
+            </IconButton>
+        </Stack>
+
+       
+        <Stack sx={imageFileContainerStackStyle}>
+            <Box
+                sx={{
+                    ...(mimeType === 'application/pdf' ? pdfFileContainerStyle : imageFileContainerStyle),
+                    transform: `scale(${zoomLevel})`,
+                    transformOrigin: 'center',
+                    transition: 'transform 0.3s'
+                }}
+            >
+                {mimeType === 'application/pdf' ? 
+                    <iframe src={fileDetails} height="500" width="100%" style={{ transform: `scale(${zoomLevel})`, transformOrigin: 'center' }}></iframe>
+                :
+                    <img src={fileDetails} style={{ ...fileImageStyle, transform: `scale(${zoomLevel})`, transformOrigin: 'center' }} alt={fileName} />
+                }
+            </Box>
+        </Stack>
+    </Stack>
+);
+};
+
 
 const DocumentView = (props) => {
     return (
