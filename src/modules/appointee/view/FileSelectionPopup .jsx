867f39f58f7852475989d8e437ab4570f82c2610
pindranil <indranil.pan@elogixmail.com>
2024-11-11 16:25:47 +0530

    add a popup modal to show the file list

--- src/modules/appointee/view/FileSelectionPopup .jsx
+++ src/modules/appointee/view/FileSelectionPopup .jsx
@@ -1,54 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Stack, Chip, Typography } from '@mui/material';
-import { popupStyle, buttonStyle, fileNameStyle } from 'app'; 
+import { popupStyle, buttonStyle, fileNameStyle, useFileChipStyles } from 'app';
 
 const FileSelectionPopup = ({ isPopupOpen, setIsPopupOpen, files, handleFileClick }) => {
-  
-    return (
-        <Dialog open={isPopupOpen} onClose={() => setIsPopupOpen(false)} maxWidth="sm" fullWidth>
-            <DialogTitle>Select a File for View</DialogTitle>
-            <DialogContent dividers>
-                <Stack
-                    direction="row"
-                    alignItems="center"
-                    justifyContent="flex-start"
-                    spacing={1}
-                    sx={popupStyle}
-                >
-                    {files.map((fileOption, index) => (
-                        <Stack
-                            key={index}
-                            direction="row"
-                            alignItems="center"
-                            justifyContent="space-between"
-                            sx={{ cursor: 'pointer', padding: '8px' }}
-                            onClick={() => {
-                                setIsPopupOpen(false);
-                                handleFileClick(fileOption);
-                            }}
-                        >
-                            <Chip
-                                label={<Typography variant="body2"  sx={{...fileNameStyle }}>{fileOption.fileName}</Typography>}
-                                variant="contained"
-                                color="secondary"
-                                sx={{ cursor: 'pointer' }}
-                            />
-                        </Stack>
-                    ))}
-                </Stack>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    onClick={() => setIsPopupOpen(false)}
-                    variant="contained"
-                    color="secondary"
-                    sx={buttonStyle}
-                >
-                    Close
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+
+  const storedViewedFiles = JSON.parse(localStorage.getItem('viewedFiles')) || [];
+
+
+  const [viewedFiles, setViewedFiles] = useState(storedViewedFiles);
+
+  useEffect(() => {
+    if (viewedFiles.length > 0) {
+      localStorage.setItem('viewedFiles', JSON.stringify(viewedFiles));
+    }
+  }, [viewedFiles]);
+
+  const handleChipClick = (fileOption) => {
+
+    if (!viewedFiles.some(file => file.fileName === fileOption.fileName)) {
+      setViewedFiles([...viewedFiles, fileOption]);
+    }
+    handleFileClick(fileOption);
+  };
+  const getChipStyles = useFileChipStyles();
+  return (
+    <Dialog open={isPopupOpen} onClose={() => setIsPopupOpen(false)} maxWidth="sm" fullWidth>
+      <DialogTitle>Select any file for view</DialogTitle>
+      <DialogContent dividers>
+        <Stack
+          direction="column"
+          alignItems="center"
+          justifyContent="flex-start"
+          spacing={1}
+          sx={{...popupStyle}}
+        >
+          {files.map((fileOption, index) => (
+            <Stack
+              key={index}
+              direction="row"
+              alignItems="center"
+              justifyContent="flex-start"
+              sx={{ cursor: 'pointer', padding: '8px', width: '100%' }}
+              onClick={() => handleChipClick(fileOption)}
+            >
+              <Chip
+                label={
+                  <Typography variant="body2" sx={{ ...fileNameStyle }}>
+                    {fileOption.fileName}
+                  </Typography>
+                }
+                variant="contained"
+                sx={getChipStyles(fileOption, viewedFiles)}
+              />
+            </Stack>
+          ))}
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => setIsPopupOpen(false)}
+          variant="contained"
+          sx={{...buttonStyle}}
+        >
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default FileSelectionPopup;
