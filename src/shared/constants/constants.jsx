f6518bc6e7533e54ba55f6f81b394d2e7f7ef2e2
elogixDibyo <dibyojit@elogixmail.com>
2024-12-04 12:50:01 +0530

    Text change

--- src/shared/constants/constants.jsx
+++ src/shared/constants/constants.jsx
@@ -2574,7 +2574,7 @@ export const timeOutMsg = `Server is down, Please try again.`;
 //export const manualSubmitConfirmatonMsg = `Are you sure you want to submit?`;
 export const manualSubmitConfirmatonMsg = `Based on your answers, Veridata will either Verify the candidate or send Issue list to candidate for reuploading the correct and required documents. Do you want to proceed & Submit?`;
 export const uploadedFromDateEmptyMsg = `"Upload From" Date cannot be empty`;
-export const FromDateEmptyMsg = `"From Date" Date cannot be empty`;
+export const FromDateEmptyMsg = `"From Date" cannot be empty`;
 export const manualverificationinfo=`You can do manual verification only in the "Manual Verification" & "Manual Re-Verification" tabs. You can't do so in the "Document Reupload Request" tab.`
 export const ManualSubmitConfirmation = ({type}) => {
  
