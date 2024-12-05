f5a6f2c5ce5eb8afca00f4dd1db5a72f0a68c10e
anushasil1234 <anusha@elogixmail.com>
2024-12-05 17:44:58 +0530

    updated darktooltip

--- src/shared/utils/tooltip/dark-tooltip.jsx
+++ src/shared/utils/tooltip/dark-tooltip.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { styled, Tooltip, tooltipClasses } from '@mui/material';
+import React from "react";
+import { styled, Tooltip, tooltipClasses } from "@mui/material";
 
 const DarkTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} arrow classes={{ popper: className }} />
+  <Tooltip {...props} arrow classes={{ popper: className }} />
 ))(({ theme }) => ({
-    [`& .${tooltipClasses.arrow}`]: {
-        color: theme.palette.common.black,
-    },
-    [`& .${tooltipClasses.tooltip}`]: {
-        backgroundColor: theme.palette.common.black,
-        boxShadow: theme.shadows[1],
-        fontSize: 16,
-        maxWidth: 500, // Set a max width
-        padding: '8px 12px', // Adjust padding
-       // wordWrap: 'break-word', // Ensure text wraps if too long
-    },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.black,
+    boxShadow: theme.shadows[1],
+    fontSize: 16,
+    maxWidth: 500,
+    padding: "8px 12px",
+  },
 }));
 
 export default DarkTooltip;
