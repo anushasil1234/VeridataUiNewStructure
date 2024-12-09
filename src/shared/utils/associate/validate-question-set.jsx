3e29c908e8fe9ea2be691b85efedca49ccbe88aa
elogixDibyo <dibyojit@elogixmail.com>
2024-12-09 13:40:15 +0530

    text change

--- src/shared/utils/associate/validate-question-set.jsx
+++ src/shared/utils/associate/validate-question-set.jsx
@@ -9,12 +9,10 @@ const validateQuestionSet = (verificationQuestionSet, verificationUpdate) => {
         for (let index = 0; index < verificationQuestionSet.length; index++) {
             const { disabled, name, subCategory, label } = verificationQuestionSet[index];
             const _name = `${name}_${subCategory}`;
-
-          
             
             if (disabled === false) {
                 if (!validationsCheck(verificationUpdate[_name], _name)) {
-                    error = `Please select the field "${label}"`;
+                    error = `Please answer the question "${label}"`;
                     break;
                 }
             }
