9a1551b61cc56a214fbd225220ace0b7532ab892
anushasil1234 <anusha@elogixmail.com>
2025-02-19 11:27:07 +0530

    share code password view added

--- src/shared/components/input-fields/text-input.jsx
+++ src/shared/components/input-fields/text-input.jsx
@@ -4,7 +4,7 @@ import Label from './label';
 import { FormControl, TextField } from '@mui/material';
 
 const TextInput = ({ value, onChange, label, required = false, readOnly = false, disabled = false, error = false, onKeyDown,onBlur,
-    onPaste, maxLength = 0, inputProps
+    onPaste, maxLength = 0, inputProps, type
 }) => {
     const _inputProps = inputProps ? inputProps : {
         maxLength: maxLength,
@@ -26,10 +26,12 @@ const TextInput = ({ value, onChange, label, required = false, readOnly = false,
                 onPaste={(e) => onPaste ? onPaste(e) : false}
                 error={error}
                 style={inputFieldStyle2}
-                type="text"
+               // type="text"
+               type={type ? type : "text"}
                 className="customeTextField"
                 variant="outlined"
-                defaultValue={" "}
+               // defaultValue={" "}
+               defaultValue={""}
                 value={value}
                 disabled={disabled}
                 inputStyle={{ padding: 0 }}
